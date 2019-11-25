8bc0394d4f7e594b702cbfd58326df2a0ba15aaf
StanMey <stan_1998@hotmail.com>
2019-11-25 23:39:39 +0100

    Implemented first solution for step 2

--- Simulator.py
+++ Simulator.py
@@ -7,30 +7,43 @@ class Simulator:
     Read https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life for an introduction to Conway's Game of Life.
     """
 
-    def __init__(self, world=None):
+    def __init__(self, rules, world=None):
         """
         Constructor for Game of Life simulator.
 
         :param world: (optional) environment used to simulate Game of Life.
         """
         self.generation = 0
+        self.rule_set = rules
         if world == None:
             self.world = World(20)
         else:
             self.world = world
 
-    def next_state(self, neighbours):
+    def get_ruleset(self, rules):
         """
+
+        :return:
+        """
+        split_rules = rules.split("/")
+        birth_rule = list(map(int, split_rules[0].split("B")[1]))
+        survive_rule = list(map(int, split_rules[1].split("S")[1]))
+
+        rule_set = birth_rule + survive_rule
+        rule_set = tuple(dict.fromkeys(sorted(rule_set)))
+        return rule_set
+
+    def next_state(self, rules, neighbours):
+        """
+        :param rules:
         :param neighbours: gets an array of all the values of the neighbours
         :return: whether a cell lives or dies
         """
         nb_amount = sum(neighbours)
-        if nb_amount < 2:
-            return 0
-        elif nb_amount > 3:
-            return 0
-        else:
+        if nb_amount in rules:
             return 1
+        else:
+            return 0
 
     def update(self) -> World:
         """
@@ -39,13 +52,15 @@ class Simulator:
         :return: New state of the world.
         """
         self.generation += 1
-        # copy the old world
+        current_rules = self.get_ruleset(self.rule_set)
+
+        # make a new world
         new_world = World(110)
 
         # TODO: Do something to evolve the generation
         for x in range(0, self.get_world().height):
             for y in range(0, self.get_world().width):
-                new_world.set(x, y, value=self.next_state(self.get_world().get_neighbours(x, y)))
+                new_world.set(x, y, value=self.next_state(current_rules, self.get_world().get_neighbours(x, y)))
 
         self.set_world(new_world)
         return self.world
